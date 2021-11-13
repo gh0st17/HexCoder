6797c0164fbab08a092d0ea0527125a6dc5fb831
Алексей <aleksey95@hotmail.com>
2021-11-13 20:09:32 +0300

    Improve macros

--- libs.hpp
+++ libs.hpp
@@ -13,14 +13,19 @@
 #include <map>
 #include <set>
 
-#ifdef _MSC_VER
-#include <intrin.h>
-#define ROL8 _rotl8
-#define ROR8 _rotr8
+#ifdef _WIN64
+  #ifdef _MSC_VER
+  #include <intrin.h>
+  #define ROL8 _rotl8
+  #define ROR8 _rotr8
+  #else
+  #include <x86intrin.h>
+  #define ROL8 _rolb
+  #define ROR8 _rorb
+  #endif
 #else
 #include <x86intrin.h>
-#define ROL8 _rolb
-#define ROR8 _rorb
+#define ROL8 __rolb
+#define ROR8 __rorb
 #endif
-
 using namespace std;
