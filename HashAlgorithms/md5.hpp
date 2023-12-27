0c2d0b0c4312878255fadf63b1b85988699c4a77
Степан <31482802+gh0st17@users.noreply.github.com>
2023-12-27 20:06:25 +0300

    Project reorganization
    - Made the project based on CMake
    - Small fixes

--- HashAlgorithms/md5.hpp
+++
@@ -1,9 +0,0 @@
-// Code from https://github.com/ulwanski/md5
-#include <string>
-#include <cstring>
-#pragma once
-
-using namespace std;
-
-string md5(string dat);
-string md5(const void* dat, size_t len);
